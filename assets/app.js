dcf443c2caa428521942fb98a4cbef99f5c380c8
Ayowande Oluwatosin <ayooluwa71@gmail.com>
2025-01-12 11:46:37 +0100

    added tailwindcss, created the html templates for the website

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,11 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+const btn = document.getElementById("hamburger-icon");
+const nav = document.getElementById("mobile-menu");
+btn.addEventListener('click', ()=>{
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+
+})
